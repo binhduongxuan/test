cb26b779777ecb3c11b84cfa1cefaf6d184b9fe9
binhdx <binhdx@newwave.vn>
2023-11-07 14:09:07 +0700

    delete

--- play.js
+++ play.js
@@ -5,22 +5,69 @@
 
 //   test(10);
 
-const person = {
-  name: 'Max',
-  age: 29,
-  greet() {
-    console.log('Hi, I am ' + this.name);
-  }
-};
+// const person = {
+//   name: 'Max',
+//   age: 29,
+//   greet() {
+//     console.log('Hi, I am ' + this.name);
+//   }
+// };  
 
-const hobbies = ['Sports','Cooking'];
-// for (let hobby of hobbies){
-//   console.log(hobby);
-// }
 
-// console.log(hobbies.map(hobby => 'Hobby: '+ hobby));
-// console.log(hobbies);
+// const printName = ({ name }) => {
+//     console.log(name);
+// };
 
-hobbies.push('Programming');
-console.log(hobbies);
+// printName(person);
 
+// const { name, age} = person;
+// console.log(name, age);
+
+// const copiedPersion = {...person};
+// console.log(copiedPersion)
+
+// const hobbies = ['Sports','Cooking'];
+// const [hobby1, hobby2] = hobbies;
+// console.log(hobby1, hobby2); 
+
+// // for (let hobby of hobbies){
+// //   console.log(hobby);
+// // }
+
+// // console.log(hobbies.map(hobby => 'Hobby: '+ hobby));
+// // console.log(hobbies);
+
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+
+// const toArray = (...args) => {
+//   return args;
+// };
+
+//==========================================================================================
+
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+
+    return promise;
+}
+
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData()
+        .then(text => {
+            console.log(text)
+            return fetchData()
+    })
+        .then(text2 => {
+            console.log(text2);
+    });
+}, 2000);
+
+console.log('Hello!');
+console.log('Hi!');
