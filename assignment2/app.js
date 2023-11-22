78d7750a34e415b9065dcde194472ce76b09f3f0
duongxuanbinh <binhdx@newwave.vn>
2023-11-22 21:51:08 +0700

    assignment2

--- assignment2/app.js
+++ assignment2/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('First middleware')
+    console.log('First middleware');
+    next();
 })
 
 app.use((req, res, next) => {
-    console.log('Second middleware')
+    console.log('Second middleware');
+    next();
 })
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>This is users page</h1>')
+});
 
 app.use('/', (req, res, next) => {
    res.send('<h1>Hello world</h1>')
