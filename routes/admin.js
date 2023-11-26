81d79a4570818c38eb155a0b745a37c1a50263df
duongxuanbinh <binhdx@newwave.vn>
2023-11-26 21:23:00 +0700

    new

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
-const express = require('express');
 const path = require('path')
-const router = express.Router();
+const express = require('express');
 const rootDir = require('../util/path');
+const router = express.Router();
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir,'views','add-product.html'))
 })
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path')
-
+const routeDir = require('../util/path')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
+    res.sendFile(path.join(routeDir, 'views', 'shop.html'))
 });
 
 
